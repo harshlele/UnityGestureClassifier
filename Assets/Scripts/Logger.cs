8a6ff5df9e52da9ab0a0a7833122270039fed286
Harshal lele <harshallele97@gmail.com>
2023-11-04 17:29:08 -0500

    changed recording interface

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -14,7 +14,10 @@ public class Logger : MonoBehaviour
     GameObject centerEye, lController, rController;
     bool recording;
     StringBuilder buff;
-    public TMP_Text hudText; 
+    public TMP_Text hudText,classInd;
+    public int classClass = 0;
+    public int classEx = 0;
+    private string folderName = "C:\\Users\\hlele\\Unity\\Record-" + DateTime.Now.ToString("MM-dd") + "_" + new System.Random().Next(1000) + "\\";
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +27,15 @@ public class Logger : MonoBehaviour
         rController = GameObject.Find("RightControllerAnchor");
         buff = new StringBuilder();
         buff.AppendLine("hd_x,hd_y,hd_z,l_x,l_y,l_z,r_x,r_y,r_z");
-      
+        Directory.CreateDirectory(folderName);
+
+       
+    }
+
+    void IncrementClass()
+    {
+        classClass += 1;
+        classEx = 0;
     }
 
     // Update is called once per frame
@@ -41,11 +52,12 @@ public class Logger : MonoBehaviour
             if (!recording)
             {
                 recording = true;
+                buff.AppendLine("hd_x,hd_y,hd_z,l_x,l_y,l_z,r_x,r_y,r_z");
             }
 
             string hPos = Regex.Replace(centerEye.transform.position.ToString(),@"\(|\)|\s","");
             string lPos = Regex.Replace(lController.transform.position.ToString(), @"\(|\)|\s", "");
-            string rPos = Regex.Replace(lController.transform.position.ToString(), @"\(|\)|\s", "");
+            string rPos = Regex.Replace(rController.transform.position.ToString(), @"\(|\)|\s", "");
 
             buff.AppendLine($"{hPos},{lPos},{rPos}");
         }
@@ -61,15 +73,32 @@ public class Logger : MonoBehaviour
             if (recording)
             {
                 recording = false;
+                classEx += 1;
                 writeLog();
             }
         }
-            
+        
+        if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+        {
+            if (!recording)
+            {
+                IncrementClass();
+            }
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        classInd.text = string.Format("Class {0} ({1} Examples)", classClass.ToString(), classEx.ToString());
     }
 
     private void writeLog()
     {
-        File.WriteAllText("C:\\Users\\hlele\\Unity\\Record-" + DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")+ ".csv", buff.ToString());
+        Directory.CreateDirectory(Path.Combine(folderName, classClass.ToString()));
+       
+        File.WriteAllText(Path.Combine(folderName,classClass.ToString(),classEx.ToString() + ".csv"), buff.ToString());
         buff.Clear();
     }
 }
