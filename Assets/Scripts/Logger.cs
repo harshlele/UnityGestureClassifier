be0e5d64fb6cb92841edf1ca9c95e34526f56ef0
Harshal lele <harshallele97@gmail.com>
2023-12-08 11:30:24 -0600

    added runner scene

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -8,7 +8,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Logger : MonoBehaviour
+public class Runner : MonoBehaviour
 {
 
     GameObject centerEye, lController, rController;
---
+++ Assets/Scripts/ModelRunner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRunner : MonoBehaviour
+{
+    GameObject centerEye, lController, rController;
+    string modelPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
